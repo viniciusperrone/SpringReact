a0cb80765d90a685cc34d76aec861d3ae6da2bb4
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-05-07 23:38:22 -0400

    added static charts

--- project-sds3/frontend/src/App.tsx
+++ project-sds3/frontend/src/App.tsx
@@ -3,15 +3,32 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import DataTable from './components/DataTable';
+import BarChart from './components/BarChart';
+import DonutChart from './components/DonutChart';
 
 const App: React.FC = () => {
   return (
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Hello World!</h1>
+        <h1 className="text-primary py-3">Dashboard</h1>
 
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas (%)</h5>
+
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Todas as vendas (%)</h5>
+
+            <DonutChart />
+          </div>
+        </div>
         <DataTable />
+      
+      
       </div>
       <Footer />
     </>
